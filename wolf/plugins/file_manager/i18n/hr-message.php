37f30d8f671dee615fc08e8582b38a1d3a08fdb3
mvdkleijn <martijn.niji@gmail.com>
2009-11-21 19:13:12 +0000

    Updated HR translation for file_manager fixes issue 83

--- wolf/plugins/file_manager/i18n/hr-message.php
+++ wolf/plugins/file_manager/i18n/hr-message.php
@@ -1,44 +1,47 @@
-<?php
+﻿<?php
 
     /**
-     * Croatian file for plugin file_manager
+     * Croatian language file for plugin file_manager
      *
      * @package wolf
      * @subpackage plugin.file_manager.translations
      *
      * @author Dejan Andjelkovic <dejan79@gmail.com>
-     * @version Wolf 0.5.5
+     * @version Wolf 0.6.0
      */
 
     return array(
-    	'A possible hack attempt was detected.' => 'Detektiran je mogući upad hakera.',
-	'Action' => 'Akcija',
-	'Are you sure you wish to delete' => 'Jeste li sigurni da želite obrisati',
+    'A possible hack attempt was detected.' => 'Mogući pokušaj hakiranja je otkriven.',
+	'Action' => 'Radnja',
+	'Are you sure you wish to delete' => 'Da li ste sigurni da želite obrisati',
 	'Cancel' => 'Odustani',
-	'Change mode' => 'Promjeni mod',
+	'Change mode' => 'chmod',
 	'Close' => 'Zatvori',
 	'Create' => 'Kreiraj',
 	'Create new directory' => 'Kreiraj novu mapu',
 	'Create new file' => 'Kreiraj novu datoteku',
 	'Directory :name has not been created!' => 'Mapa :name nije kreirana!',
-	'Directory is not writable! File has not been saved!' => 'U mapu se ne može zapisivati! Datoteka nije spremljena!',
+	'Directory is not writable! File has not been saved!' => 'U mapu nije moguće zapisivati! Datoteka nije spremljena!',
 	'File' => 'Datoteka',
 	'File :name has been created with success!' => 'Datoteka :name je uspješno kreirana!',
 	'File :name has not been created!' => 'Datoteka :name nije kreirana!',
+	'File Manager' => 'Upravitelj datotekama',
 	'File has been saved with success!' => 'Datoteka je uspješno spremljena!',
 	'File has not been uploaded!' => 'Datoteka nije učitana!',
-	'File is not writable! File has not been saved!' => 'U datoteku se ne može zapisivati! Datoteka nije spremljena!',
-	'File or directory not found!' => 'Datoteka ili mapa nisu pronađene!',
-	'Modify' => 'Izmjenjeno',
+	'File is not writable! File has not been saved!' => 'U datoteku nije moguće pisati! Datoteka nije spremljena!',
+	'File or directory not found!' => 'Datoteka ili mapa nije pronađena!',
+	'Files' => 'Datoteke',
+	'Modify' => 'Izmijeni',
 	'Permission denied!' => 'Dozvola je odbijena!',
 	'Permissions' => 'Dozvole',
-	'Rename' => 'Preimenuj',
+	'Provides interface to manage files from the administration.' => 'Omogućuje sučelje za upravljanje datotekama iz administracije.',
+	'Rename' => 'Preimenovati',
 	'Save' => 'Spremi',
-	'Save and Continue Editing' => 'Spremi i Nastavi uređivati',
+	'Save and Continue Editing' => 'Spremi i Nastavi Uređivati',
 	'Size' => 'Veličina',
 	'Upload' => 'Učitaj',
 	'Upload file' => 'Učitaj datoteku',
-	'You do not have permission to access the requested page!' => 'Nemate ovlasti za pristup stranici!',
+	'You do not have permission to access the requested page!' => 'Nemate dozvolu za pristup traženoj stranici!',
 	'or' => 'ili',
-	'overwrite it?' => 'prepiši',
+	'overwrite it?' => 'prepisati?',
     );
