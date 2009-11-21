23a587cf0c7567335a9c835a860403adafc03eb2
mvdkleijn <martijn.niji@gmail.com>
2009-11-21 19:14:31 +0000

    Added hr translation for comment plugin Fixes issue 84

---
+++ wolf/plugins/comment/i18n/hr-message.php
@@ -0,0 +1,66 @@
+﻿<?php
+
+    /**
+     * Croatian language file for plugin comment
+     *
+     * @package wolf
+     * @subpackage plugin.comment.translations
+     *
+     * @author Dejan Andjelkovic <dejan79@gmail.com>
+     * @version Wolf 0.6.0
+     */
+
+    return array(
+    'A comment was added.' => 'Komentar je dodan.',
+	'Approve' => 'Odobriti',
+	'Are you sure you wish to delete it?' => 'Da li ste sigurni da želite obrisati?',
+	'Auto approve' => 'Odobri automatski',
+	'Body' => 'Tijelo',
+	'Cancel' => 'Odustani',
+	'Choose yes if you want to display the number of to-be-moderated &amp; total number of comment in the Comment plugin\'s tab.' => 'Odaberite da ako želite prikazati broj za uređivanje &amp; ukupni broj komentara u kartici Komentari.',
+	'Choose yes if you want to use a captcha to protect yourself against spammers.' => 'Odaberite da ako želite koristiti captcha kako biste se zaštitili od spamera',
+	'Choose yes if you want your comments to be auto approved. Otherwise, they will be placed in the moderation queue.' => 'Odaberite da ako želite da se komentari automatski odobravaju. U suprotnom će biti stavljeni na čekanje.',
+	'Closed' => 'Isključi',
+	'Comment has been approved!' => 'Komentar je odobren!',
+	'Comment has been deleted!' => 'Komentar je obrisan!',
+	'Comment has been saved!' => 'Komentar je spremljen!',
+	'Comment has been unapproved!' => 'Komentar nije odobren!',
+	'Comment has not been deleted!' => 'Komentar nije obrisan!',
+	'Comment has not been saved!' => 'Komentar nije spremljen!',
+	'Comment not found!' => 'Komentar nije pronađen!',
+	'Comments' => 'Komentari',
+	'Comments Plugin' => 'Komentari Plugin',
+	'Comments per page' => 'Komentara po stranici',
+	'Comments settings' => 'Postavke komentara',
+	'Delete' => 'Obrisati',
+	'Documentation' => 'Dokumentacija',
+	'Edit comment' => 'Uredi komentar',
+	'Enhance comments tab' => 'Povećaj karticu komentara',
+	'Incorrect result value. Please try again:' => 'Netočna vrijednost rezultata. Pokušajte ponovno:',
+	'Moderation' => 'Uređivanje',
+	'Modify' => 'Uredi',
+	'No' => 'Ne',
+	'No comments found for moderation.' => 'Nema komentara za uređivanje.',
+	'No comments found.' => 'Nema komentara',
+	'Open' => 'Uključi',
+	'Please insert the result of the arithmetical operation from the following image:' => 'Unesite rezultat aritmetičke operacije sa slike:',
+	'Please insert the result of the arithmetical operation from this image.' => 'Unesite rezultat aritmetičke operacije sa ove slike.',
+	'Provides interface to add page comments.' => 'Omogućuje sučelje za dodavanje stranice s komentarima.',
+	'Reject' => 'Odbaci',
+	'Save' => 'Spremi',
+	'Sets the number of comments to be displayed per page in the backend.' => 'Postavlja broj komentara po stranici koji će se prikazivati u backendu.',
+	'Settings' => 'Postavke',
+	'Successfully uninstalled plugin.' => 'Plugin je uspješno deinstaliran.',
+	'Thank you for your comment. It has been added.' => 'Zahvaljujemo se na vašem komentaru. Komentar je dodan.',
+	'Thank you for your comment. It is waiting for approval.' => 'Zahvaljujemo se na vašem komentaru. Kometar čeka odobrenje administratora.',
+	'The settings have been updated.' => 'Postavke su ažurirane.',
+	'Unable to clean up table alterations.' => 'Nije moguće obrisati promjene u tablici.',
+	'Unable to delete plugin settings.' => 'Nije moguće obrisati postavke plugina.',
+	'Unable to drop table :tablename' => 'Nije moguće obrisati tablicu :tablename',
+	'Use captcha' => 'Koristi captcha',
+	'Yes' => 'Da',
+	'about' => 'o',
+	'comment not found!' => 'komentar nije pronađen',
+	'none' => 'ništa',
+	'or' => 'ili',
+    );
