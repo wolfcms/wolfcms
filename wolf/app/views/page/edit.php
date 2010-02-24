866e865d77da2b9f1ae8cab0c9a85d809382ef09
mvdkleijn <martijn.niji@gmail.com>
2010-02-24 22:55:58 +0000

    Fixed a problem with inherit option for layout. (might be DB bug)

--- wolf/app/views/page/edit.php
+++ wolf/app/views/page/edit.php
@@ -99,7 +99,7 @@ if ($action == 'edit') { ?>
                   <td class="label"><label for="page_layout_id"><?php echo __('Layout'); ?></label></td>
                   <td class="field">
                       <select id="page_layout_id" name="page[layout_id]">
-                        <option value="">&#8212; <?php echo __('inherit'); ?> &#8212;</option>
+                        <option value="0">&#8212; <?php echo __('inherit'); ?> &#8212;</option>
                       <?php foreach ($layouts as $layout): ?>
                         <option value="<?php echo $layout->id; ?>"<?php echo $layout->id == $page->layout_id ? ' selected="selected"': ''; ?>><?php echo $layout->name; ?></option>
                       <?php endforeach; ?>
