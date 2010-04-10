4f4c8b670c6a32cd71de183f2f7344c09941f92a
dajare <dajare@gmail.com>
2010-04-10 20:46:06 +0000

    Added Slovak translation for core and file manager plugin - thanks joe.hergott - issues 163, 164

---
+++ wolf/plugins/file_manager/i18n/sk-message.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Slovak language file for plugin file_manager
+ *
+ * @package wolf
+ * @subpackage plugin.file_manager.translations
+ *
+ * @author Jozef Hergott
+ * @version Wolf 0.6.0
+ */
+
+return array(
+'A possible hack attempt was detected.' => '',
+'Action' => 'Akcia',
+'Are you sure you wish to delete?' => 'Skutočne chcete zmazať?',
+'Cancel' => 'Zrušiť',
+'Change mode' => 'Zmeniť mód',
+'Close' => 'Zatvoriť',
+'Create' => 'Vytvoriť',
+'Create new directory' => 'Vytvoriť nový priečinok',
+'Create new file' => 'Vytvoriť nový súbor',
+'Delete file' => 'Zmazať súbor',
+'Directory :name has not been created!' => 'Priečinok :name bol vytvorený!',
+'Directory is not writable! File has not been saved!' => 'Priečinok nie je zapisovateľný! Súbor nebol uložený',
+'File' => 'Súbor',
+'File :name has been created with success!' => 'Súbor :name bol úspešne vytvorený!',
+'File :name has not been created!' => 'Vytvorenie súboru :name zlyhalo!',
+'File Manager' => 'Správca súborov',
+'File has been saved with success!' => 'Súbor bol úspešne uložený!',
+'File has not been uploaded!' => 'Uloženie súboru zlyhalo!',
+'File is not writable! File has not been saved!' => 'Súbor nie je zapisovateľný! Súbor nebol uložený!',
+'File or directory not found!' => 'Súbor alebo priečinok nenájdený!',
+'Files' => 'Súbory',
+'Modify' => 'Upraviť',
+'Permission denied!' => 'Prístup zamietnutý!',
+'Permissions' => 'Prístupové práva',
+'Provides interface to manage files from the administration.' => 'Poskytuje rozharanie na správu súborov.',
+'Rename' => 'Premenovať',
+'Save' => 'Uložiť',
+'Save and Continue Editing' => 'Uložiť a pokračovať v úpravách',
+'Size' => 'Veľkosť',
+'Upload' => 'Nahrať',
+'Upload file' => 'Nahrať súbor',
+'You do not have permission to access the requested page!' => 'Nemáte práva na prístup k požadovanej stránke!',
+'delete file icon' => 'ikona zmazanie súboru',
+'or' => 'alebo',
+'overwrite it?' => 'prepísať?'
+
+);
+?>
