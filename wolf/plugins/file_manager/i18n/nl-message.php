30cb77cf64ebd691a470c47f19820acfc0cf23c9
dajare <dajare@gmail.com>
2010-04-11 08:55:16 +0000

    Updated Dutch translation for core and file manager - thanks Fortron

--- wolf/plugins/file_manager/i18n/nl-message.php
+++ wolf/plugins/file_manager/i18n/nl-message.php
@@ -3,42 +3,49 @@
 /**
  * Dutch language file for plugin file_manager
  *
- * @package plugins
- * @subpackage file_manager
- * @category translations
+ * @package wolf
+ * @subpackage plugin.file_manager.translations
  *
  * @author Martijn van der Kleijn <martijn.niji@gmail.com>
  * @version Wolf 0.9.4
  */
 
     return array(
-        'Action' => 'Aktie',
-        'Are you sure you wish to delete' => 'Weet u zeker dat u wilt verwijderen?',
-        'Cancel' => 'Annuleer',
-        'Change mode' => 'Wijzig de modus',
-        'Close' => 'Sluit',
-        'Create' => 'Maak aan',
-        'Create new directory' => 'Creëer nieuwe directory',
-        'Create new file' => 'Creëer nieuw bestand',
-        'Directory :name has not been created!' => 'De directory <strong>:name</strong> is niet aangemaakt!',
-        'Directory is not writable! File has not been saved!' => 'Directory is niet schrijfbaar! Bestand is niet bewaard!',
-        'File' => 'Bestand',
-        'File :name has been created with success!' => 'Het bestand <strong>:name</strong> is successvol aangemaakt!',
-        'File :name has not been created!' => 'Het bestand <strong>:name</strong> is niet aangemaakt!',
-        'File has been saved with success!' => 'Bestand is met success bewaard!',
-        'File has not been uploaded!' => 'Het bestand is niet geupload!',
-        'File is not writable! File has not been saved!' => 'Bestand is niet schrijfbaar! Bestand is niet bewaard!',
-        'File or directory not found!' => 'Het bestand of de directory werdt niet gevonden!',
-        'Modify' => 'Wijzig',
-        'Permission denied!' => 'Toegang geweigerd!',
-        'Permissions' => 'Machtigingen',
-        'Rename' => 'Hernoem',
-        'Save' => 'Bewaar',
-        'Save and Continue Editing' => 'Bewaar en Blijf Bewerken',
-        'Size' => 'Grootte',
-        'Upload' => 'Upload',
-        'Upload file' => 'Upload bestand',
-        'You do not have permission to access the requested page!' => 'U heeft onvoldoende toestemming om the gevraagde pagina te benaderen!',
-        'or' => 'of',
-        'overwrite it?' => 'overschrijven?',
+    'A possible hack attempt was detected.' => 'Er is een mogelijke hack poging gededecteerd.',
+	'Action' => 'Aktie',
+	'Are you sure you wish to delete?' => 'Weet u zeker dat u wilt verwijderen?',
+	'Cancel' => 'Annuleer',
+	'Change mode' => 'Wijzig de modus',
+	'Close' => 'Sluit',
+	'Create' => 'Maak aan',
+	'Create new directory' => 'Creëer nieuwe directory',
+	'Create new file' => 'Creëer nieuw bestand',
+	'Delete file' => 'Verwijder bestand',
+	'Directory :name has not been created!' => 'De directory <strong>:name</strong> is niet aangemaakt!',
+	'Directory is not writable! File has not been saved!' => 'Directory is niet schrijfbaar! Bestand is niet bewaard!',
+	'File' => 'Bestand',
+	'File :name has been created with success!' => 'Het bestand <strong>:name</strong> is successvol aangemaakt',
+	'File :name has not been created!' => 'Het bestand <strong>:name</strong> is niet aangemaakt!',
+	'File Manager' => 'Bestandsbeheer',
+	'File has been saved with success!' => 'Bestand is met success bewaard!',
+	'File has not been uploaded!' => 'Het bestand is niet geupload!',
+	'File is not writable! File has not been saved!' => 'Bestand is niet schrijfbaar! Bestand is niet bewaard!',
+	'File or directory not found!' => 'Het bestand of de directory werdt niet gevonden!',
+	'Files' => 'Bestanden',
+	'Modify' => 'Wijzig',
+	'Permission denied!' => 'Toegang geweigerd!',
+	'Permissions' => 'Toegangsrechten',
+	'Provides interface to manage files from the administration.' => '',
+	'Rename' => 'Hernoem',
+	'Save' => 'Bewaar',
+	'Save and Continue Editing' => 'Bewaar en Blijf Bewerken',
+	'Size' => 'Grootte',
+	'Upload' => 'Upload',
+	'Upload file' => 'Upload bestand',
+	'You do not have permission to access the requested page!' => 'Je hebt niet de bevoegdheid om deze pagina te bekijken!',
+	'delete file icon' => 'Verwijder bestand icon',
+	'or' => 'of',
+	'overwrite it?' => 'overschrijven?',
     );
+	
+	
