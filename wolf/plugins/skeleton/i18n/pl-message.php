3c06b68ce8fdb227bc1c37878534f36220ea7f97
dajare <dajare@gmail.com>
2010-04-26 20:14:11 +0000

    Added Polish translation for Skeleton plugin - thanks Piotr.Fuz - issue 89

---
+++ wolf/plugins/skeleton/i18n/pl-message.php
@@ -0,0 +1,24 @@
+﻿<?php
+
+    /**
+     * YourLanguage file for plugin skeleton
+     *
+     * @package wolf
+     * @subpackage plugin.skeleton.translations
+     *
+     * @author Piotr Fuz <piotr.fuz@gmail.com>
+     * @version Wolf 0.6
+     */
+
+    return array(
+    'A sidebar' => 'Panel boczny',
+	'Display settings page here!' => 'Tutaj wyświetl stronę z ustawieniami!',
+	'Display your documentation here!' => 'Tutaj wyświetl dokumentację!',
+	'Documentation' => 'Dokumentacja',
+	'Provides a basic plugin implementation. (try enabling it!)' => 'Umożliwia podstawową implementację dodatków. (spróbuj to włączyć!)',
+	'Put something here, or leave out the sidebar entirely.' => 'Dodaj coś w panelu bocznym lub zostaw cały pusty.',
+	'Settings' => 'Ustawienia',
+	'Skeleton' => 'Szkielet',
+    );
+
+
