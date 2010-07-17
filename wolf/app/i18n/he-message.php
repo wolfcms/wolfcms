6d20ca097220c0ca6bc4e16885c094d7055dc1ff
mvdkleijn <martijn.niji@gmail.com>
2010-07-17 19:00:07 +0000

    Added dutch translation for comment plugin - Thanks Rinus - issue 187

---
+++ wolf/app/i18n/he-message.php
@@ -0,0 +1,195 @@
+<?php
+
+/**
+ * Hebrew language file
+ *
+ * @package wolf
+ * @subpackage translations
+ *
+ * @author Philippe Archambault
+ * @version Wolf x.y.z
+ *
+ * Note: original translator translated a Frog file. Next time it should follow
+ * the TranslatorNotes as states in the Wiki.
+ */
+
+return array(
+
+// general
+'Pages' => 'דפים',
+'Snippets' => 'קטעי קוד',
+'Layouts' => 'תבניות',
+'Files' => 'קבצים',
+'Users' => 'משתמשים',
+'Modify' => 'עריכה',
+'Action' => 'פעולה',
+'Close' => 'סגירה',
+'Save' => 'שמירה',
+'Save and Close' => 'שמירה וסגירה',
+'Save and Continue Editing' => 'שמירה והמשך עריכה',
+'or' => 'או',
+'Cancel' => 'ביטול',
+'Name' => 'שם',
+'Last updated by' => 'עודכן לאחרונה ע"י',
+'on' => 'על',
+'Upload' => 'העלאת קובץ',
+'Are you sure you wish to delete' => 'למחוק?',
+'Published' => 'פורסם',
+'Draft' => 'טיוטה',
+'Reviewed' => 'נערך',
+'Hidden' => 'מוסתר',
+'More' => 'עוד',
+'Less' => 'הסתרה',
+'Add' => 'הוספה',
+'Required.' => 'נדרש.',
+'Optional.' => 'אופציונלי.',
+'yes' => 'כן',
+'no' => 'לא',
+
+
+// layouts
+'Layout' => 'תבנית',
+'New Layout' => 'תבנית חדשה',
+'What is a Layout?' => 'מהי תבנית?',
+'Use layouts to apply a visual look to a Web page. Layouts can contain special tags to include
+  page content and other elements such as the header or footer. Click on a layout name below to
+  edit it or click <strong>Remove</strong> to delete it.' =>
+'השתמשו בתבניות בכדי לעצב דפי אינטרנט. תבניות יכולות להכיל תגיות מיוחדות המכניסות תוכן לדף וכן אלמנטים נוספים כגון כותרת ו-footer. הקליקו על שם תבנית על מנת לערוך אותה, או על <strong>מחיקה</strong> על מנת למחוק אותה.',
+'Add Layout' => 'הוספת תבנית',
+'Edit Layout' => 'עריכת תבנית',
+'Remove Layout' => 'מחיקת תבנית',
+'inherit' => 'ירושה',
+
+// pages
+'Page' => 'דף',
+'reorder' => 'שינוי סדר דפים',
+'Add Page' => 'הוספת דף',
+'Edit Page' => 'עריכת דף',
+'Status' => 'סטטוס',
+'Order' => 'סדר',
+'Add child' => 'הוספת בן',
+'Add Tab' => 'הוספת טאב',
+'Add Part' => 'הוספת חלק',
+'Remove Tab' => 'הסרת טאב',
+'Delete the current tab?' => 'למחוק את הטאב הנוכחי?',
+'Remove page' => 'מחק דף',
+'Remove page disable' => 'הפיכת דף לפעיל',
+'Drag and Drop' => 'גרור והדבק',
+'Drag and Drop disable' => 'ביטול אפשרות גרירה',
+'Page Title' => 'כותרת דף',
+'Metadata' => 'metadata',
+'Filter' => 'מסנן',
+'none' => 'ללא',
+'Page Type' => 'סוג הדף',
+'Comments' => 'תגובות',
+
+// settings
+'Settings' => 'הגדרות',
+'Admin Site title' => 'כותרת ממשק ניהול',
+'Language' => 'שפה',
+'Administration Theme' => 'תבנית ממשק ניהול',
+'Page options' => 'אפשרויות דף',
+'Default Status' => 'מצב ברירת מחדל',
+'Default Filter' => 'Filtre par défaut',
+'Optional component' => 'רכיב אופציונלי',
+'Enable comments' => 'לאפשר תגובות',
+'Auto approve comments' => 'אישור תגובות אוטומטי',
+'Display stats' => 'הצגת סטטיסטיקה',
+'Display file manager' => 'הצגת מנהל קבצים',
+'By using <strong>&lt;img src="img_path" /&gt;</strong> you can set your company logo instead of a title.' => 'ניתן להציג תמונה במקום הכותרת ע"י שימוש ב <strong>&lt;img src="img_path" /&gt;</strong>',
+'This will set your language for the backend.' => 'כאן ניתן לבחור את שפת ממשק הניהול.',
+'This will change your Administration theme.' => 'כאן ניתן לשנות את תבנית ממשק הניהול',
+'Only for filter in pages, NOT in snippets' => 'מיועד לסינון תוכן הדף, לא לשימוש בקטעי קוד.',
+
+// snippets
+'New Snippet' => 'קטע קוד חדש',
+'What is a Snippet?' => 'מה זה קטע קוד?',
+'Snippets are generally small pieces of content which are included in other pages or layouts.' =>
+'קטעי קוד הם תוכניות php קטנות המציגות תוכן וניתנות להטמעה בתוך תבניות',
+'Snippet' => 'קטע קוד',
+'Add Snippet' => 'הוספת קטע קוד',
+'Edit Snippet' => 'עריכת קטע קוד',
+'Remove Snippet' => 'מחיקת קטע קוד',
+
+
+// users
+'Add User' => 'הוספת משתמש',
+'Edit user' => 'עריכת משתמש',
+'Username' => 'שם משתמש',
+'E-mail' => 'אימייל',
+'Password' => 'סיסמה',
+'Forgot password?' => 'שכחת סיסמה?',
+'Forgot password' => 'שכחת את הסיסמה',
+'Email address:' => 'כתובת אימייל:',
+'Confirm Password' => 'אישור סיסמה',
+'Send password' => 'שליחת סיסמה',
+'No user found!' => 'משתמש לא נמצא',
+'Login' => 'כניסה',
+'Remember me for 14 days' => 'זכור אותי למשך 14 יום',
+'Roles' => 'תפקידים',
+'Administrator' => 'מנהל',
+'Developer' => 'מפתח',
+'Editor' => 'עורך תוכן',
+'Optional. Please use a valid e-mail address.' => 'אופציונלי. יש להכניס כתובת אימייל קיימת',
+'At least 3 characters. Must be unique.' => '3 אותיות לפחות.',
+'At least 5 characters.' => '5 אותיות לפחות',
+'Leave password blank for it to remain unchanged.' => 'יש להשאיר את הסיסמה ריקה אם אין ברצונך לשנותה.',
+'Roles restrict user privileges and turn parts of the administrative interface on or off.' => 'תפקידים מאפשרים להגביל הרשאות משתמשים וכן להפעיל/לבטל חלקים שונים בממשק הניהול',
+'New User' => 'משתמש חדש',
+'Where do the avatars come from?' => 'מהיכן נלקחים האווטארים?',
+'The avatars are automatically linked for those with a <a href="http://www.gravatar.com/" target="_blank">Gravatar</a> (a free service) account.' => 'אווטארים מקושרים אוטומטית לחשבון <a href="http://www.gravatar.com/" target="_blank">Gravatar</a>',
+
+
+// errors and success
+'Layout has been added!' => 'התבנית נשמרה בהצלחה',
+'Layout has not been added. Name must be unique!' => 'התבנית לא הוספה. שם התבנית חייב להיות יחודי!',
+'Layout has been saved!' => 'התבנית לא נשמרה!',
+'Layout has not been saved. Name must be unique!' => 'התבנית לא נשמרה. שם התבנית חייב להיות יחודי!',
+'Layout :name has been deleted!' => 'התבנית :name נמחקה בהצלחה',
+'Layout :name has not been deleted!' => 'התבנית :name לא נמחקה!',
+'Layout not found!' => 'התבנית לא נמצאה!',
+
+'Login failed. Please check your login data and try again.' => 'הכניסה למערכת נכשלה. אנא בדקו המידע ונסו שנית',
+
+'Page has been saved!' => 'הדף נשמר בהצלחה!',
+'Page has not been saved!' => 'הדף לא נשמר!',
+'Page not found!' => 'הדף לא נמצא!',
+'Page :title has been deleted!' => 'הדף :title נמחק בהצלחה!',
+'Page :title has not been deleted!' => 'הדף :title לא נמחק!',
+'Action disabled!' => 'הפעולה המבוקשת אינה פעילה!',
+
+'Snippet has been added!' => 'הוספת קטע קוד התבצעה',
+'Snippet has not been added. Name must be unique!' => 'הוספת קטע קוד לא הצליחה',
+'Snippet :name has been saved!' => 'הוספת קטע הקוד :name התבצעה בהצלחה!',
+'Snippet :name has not been saved. Name must be unique!' => 'קטע הקוד :name לא נשמר. כבר הוגדר קטע קוד בשם זה!',
+'Snippet :name has been deleted!' => 'קטע הקוד :name נשמר בהצלחה!',
+'Snippet :name has not been deleted!' => 'קטע הקוד :name נמחק בהצלחה!',
+'Snippet not found!' => 'קטע קוד לא נמצא!',
+
+'You do not have permission to access the requested page!' => 'אין לך הרשאה מתאימה בכדי להיכנס לדף המבוקש!',
+'Password and Confirm are not the same or too small!' => 'הסיסמה ואישור הסיסמה אינן זהות או קטנות מדי!',
+'Username must contain a minimum of 3 characters!' => 'על שם המשתמש להיות באורך של 3 תווים לפחות!',
+'User has been added!' => 'הוספת משתמש התבצעה בהצלחה!',
+'User has not been added!' => 'הוספת משתמש נכשלה!',
+'User not found!' => 'המשתמש לא נמצא!',
+'User has been saved!' => 'שמירת משתמש התבצעה בהצלחה!',
+'User has not been saved!' => 'שמירת משתמש נכשלה!',
+'User <strong>:name</strong> has been deleted!' => 'המשתמש :name נמחק בהצלחה!',
+'User <strong>:name</strong> has not been deleted!' => 'מחיקת המשתמש :name נכשלה!',
+
+// layout
+'You are currently logged in as' => 'נכנסת למערכת',
+'Log Out' => 'יציאה',
+'View Site' => 'לצפייה באתר',
+'Thank you for using' => 'תודה על השימוש',
+'version' => 'גירסה',
+'Page rendered in' => 'הדף נוצר תוך',
+'seconds' => 'שניות',
+'Memory usage:' => 'צריכת זיכרון:',
+
+// Comments
+'Delete' => 'מחיקה',
+'Reject' => 'דחייה',
+'Approve' => 'אישור'
+
+);
