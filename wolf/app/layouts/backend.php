b4b95badc5bae4fb9a1f6020326fa565169696da
snsmurf <snsmurf@gmail.com>
2010-10-11 21:31:31 +0000

    Updating the jquery UI file again

--- wolf/app/layouts/backend.php
+++ wolf/app/layouts/backend.php
@@ -74,7 +74,7 @@ if (isset($this->vars['content_for_layout']->vars['action'])) {
     <script type="text/javascript" charset="utf-8" src="<?php echo URI_PUBLIC; ?>wolf/admin/javascripts/wolf.js"></script>
     <!--script type="text/javascript" charset="utf-8" src="<?php echo URI_PUBLIC; ?>wolf/admin/javascripts/control.textarea.js"></script-->
     <script type="text/javascript" charset="utf-8" src="<?php echo URI_PUBLIC; ?>wolf/admin/javascripts/jquery-1.4.2.min.js"></script>
-    <script type="text/javascript" charset="utf-8" src="<?php echo URI_PUBLIC; ?>wolf/admin/javascripts/jquery-ui-1.8.2.custom.min.js"></script>
+    <script type="text/javascript" charset="utf-8" src="<?php echo URI_PUBLIC; ?>wolf/admin/javascripts/jquery-ui-1.8.5.custom.min.js"></script>
     <script type="text/javascript" charset="utf-8" src="<?php echo URI_PUBLIC; ?>admin/javascripts/jquery.cookies.js"></script>
     <script type="text/javascript" charset="utf-8">
      var $j = jQuery.noConflict();
