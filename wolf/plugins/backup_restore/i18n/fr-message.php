aaecf438be56ab13524206fe5bd78fafc7607f1e
mvdkleijn <martijn.niji@gmail.com>
2010-12-12 01:57:04 +0000

    Added French translation for backup restore plugin - thanks oweb

---
+++ wolf/plugins/backup_restore/i18n/fr-message.php
@@ -0,0 +1,52 @@
+<?php
+
+    /**
+     * Frenc file for plugin backup_restore
+     *
+     * @package wolf
+     * @subpackage plugin.backup_restore.translations
+     *
+     * @author Martijn van der Kleijn <martijn.niji@gmail.com>
+     * @version Wolf 0.5.5
+     */
+
+    return array(
+    	'Are you sure you wish to restore?' => 'Etes-vous sûr de vouloir restaurer ?',
+        //'Backup Restore' => '',
+        'Backup settings' => 'Réglages sauvegarde',
+        //'Backup/Restore plugin' => '',
+        'Create a backup' => 'Générer une sauvegarde',
+        'Creating the backup' => 'Création de sauvegarde',
+        'Current style' => 'Style courant',
+        'Designed for Wolf version' => 'Conçu pour Wolf CMS version',
+        'Do you want to download the backup as a zip file?' => 'Voulez-vous télécharger la sauvegarde en fichier compressé ?',
+        'Do you want to include passwords in the backup file? <br/> If you select no, all passwords will be reset upon restoring the backup.' => 'Voulez-vous inclure les mots de passe dans la sauvegarde ? <br/>Si vous choisissez non, tous les mots de passe seront réinitialisés lors de la restauration de la sauvegarde.',
+        'Documentation' => 'Documentation',
+        'Filename timestamp style' => 'Format de date pour le fichier',
+        'If no password is provided in the backup file, reset all password fields to this default.' => 'Si aucun mot de passe n\'est indiqué dans la sauvegarde, réinitialiser tous les mots de passe à celui indiqué par défaut.',
+        'Include passwords' => 'Ajouter mots de passe',
+        'No' => 'Non',
+        'Notes' => 'Notes',
+        'Package as zip file' => 'Générer une archive ZIP',
+        'Provides administrators with the option of backing up their pages and settings to an XML file.' => 'Provides administrators with the option of backing up their pages and settings to an XML file.',
+        'Reset passwords to' => 'Mot de passe de réinitialisation',
+        'Restore a backup' => 'Restaurer une sauvegarde',
+        'Restore settings' => 'Réglages pour restauration',
+        'Restoring a backup' => 'Restauration de sauvegarde',
+        'Save' => 'Enregistrer',
+        'Settings' => 'Réglages',
+        'Succesfully restored backup.' => 'Sauvegarde restaurée correctement.',
+        'Successfully uninstalled plugin.' => 'Plugin désinstallé.',
+        'The Backup/Restore plugin allows you to create complete backups of the Wolf CMS core database.' => 'Le plugin Backup/Restore vous permet de générer une sauvgarde complète de la base de données du noyau de Wolf CMS.',
+        'The settings have been saved.' => 'Les réglages ont été enregistrés.',
+        'This is an example of the filename that will be used for the generated XML file.' => 'Ceci est un exemple du nom du fichier qui sera utilisé pour le fichier XML généré.',
+        'Unable to reconstruct table :tablename.' => 'Impossible de reconstruire la table :tablename.',
+        'Unable to remove plugin settings.' => 'Impossible de supprimer les réglages du plugin.',
+        'Unable to truncate current table :tablename.' => 'Niet in staat om tabel :tablename leeg te gooien.',
+        'Upload file' => 'Upload bestand',
+        'Version' => 'Version actuelle',
+        'Warning!' => 'Attention !',
+        'What style of timestamp should be encorporated into the filename.' => 'Quel style de date doit être intégré dans le nom de fichier.',
+        'Yes' => 'Oui',
+        'You do not have permission to access the requested page!' => 'Vous ne disposez pas des permissions pour accéder à cette page !',
+    );
