b7c520715dde8aa1028081836082cdb1f1640fb6
andrewmman <andrewmman@gmail.com>
2011-01-02 11:14:42 -0800

    includes new translatable strings

--- wolf/plugins/backup_restore/i18n/es-message.php
+++ wolf/plugins/backup_restore/i18n/es-message.php
@@ -3,36 +3,37 @@
     /**
      * Spanish file for plugin backup_restore
      *
-     * @package Plugins
+     * @package plugins
      * @subpackage backup_restore
+     * @category translations
      *
      * @author andrewmman
-     * @version Wolf 0.6
+     * @version Wolf 0.7
      */
 
     return array(
-    	'Are you sure you wish to restore?' => '¿Estás seguro que quieres restaurar?',
+    	'Are you sure you wish to restore?' => '&iquest;Est&aacute;s seguro que quieres restaurar?',
         'Backup Restore' => 'Backup Restore',
         'Backup settings' => 'ajustes de Respaldo',
         'Backup/Restore plugin' => 'plugin Backup/Restore',
         'Create a backup' => 'Crear un respaldo',
         'Creating the backup' => 'Creando el respaldo',
         'Current style' => 'Estilo actual',
-        'Designed for Wolf version' => 'Diseñado para la versión de Wolf',
-        //'Designed for Wolf version :v and upwards.' => 'Diseñado para la versión :v de Wolf y superiores.',
-        'Do you want to download the backup as a zip file?' => '¿Quieres descargar el respaldo como un archivo zip?',
-        'Do you want to include passwords in the backup file? <br/> If you select no, all passwords will be reset upon restoring the backup.' => '¿Quieres incluir las contraseñas en el archivo de respaldo?<br/>En caso negativo, todas las contraseñas serán eliminadas cuando restaures el respaldo.',
-        'Documentation' => 'Documentación',
+        'Designed for Wolf version' => 'Dise&ntilde;ado para la versi&oacute;n de Wolf',
+        //'Designed for Wolf version :v and upwards.' => 'Dise&ntilde;ado para la versi&oacute;n :v de Wolf y superiores.',
+        'Do you want to download the backup as a zip file?' => '&iquest;Quieres descargar el respaldo como un archivo zip?',
+        'Do you want to include passwords in the backup file? <br/> If you select no, all passwords will be reset upon restoring the backup.' => '&iquest;Quieres incluir las contrase&ntilde;as en el archivo de respaldo?<br/>En caso negativo, todas las contrase&ntilde;as ser&aacute;n eliminadas cuando restaures el respaldo.',
+        'Documentation' => 'Documentaci&oacute;n',
         'Filename timestamp style' => 'Estilo de la fecha de registro',
-        'If no password is provided in the backup file, reset all password fields to this default.' => 'En el caso de no proporcionar una contraseña en el archivo de respaldo, reiniciar todos los campos de contraseña con esta contraseña.',
-        'Include passwords' => 'Incluir contraseñas',
+        'If no password is provided in the backup file, reset all password fields to this default.' => 'En el caso de no proporcionar una contrase&ntilde;a en el archivo de respaldo, reiniciar todos los campos de contrase&ntilde;a con esta contrase&ntilde;a.',
+        'Include passwords' => 'Incluir contrase&ntilde;as',
         'No' => 'No',
         'Notes' => 'Notas',
         'Package as zip file' => 'Empaquetar como archivo zip',
-        'Provides administrators with the option of backing up their pages and settings to an XML file.' => 'Provee a los administradores, la opción de respaldar las páginas y ajustes a un archivo XML.',
-        'Reset passwords to' => 'Reiniciar contraseña a',
+        'Provides administrators with the option of backing up their pages and settings to an XML file.' => 'Provee a los administradores, la opci&oacute;n de respaldar las p&aacute;ginas y ajustes a un archivo XML.',
+        'Reset passwords to' => 'Reiniciar contrase&ntilde;a a',
         'Restore a backup' => 'Restaurar un respaldo',
-        'Restore settings' => 'Ajustes de restauración',
+        'Restore settings' => 'Ajustes de restauraci&oacute;n',
         'Restoring a backup' => 'Restaurando un respaldo',
         'Save' => 'Guardar',
         'Settings' => 'Ajustes',
@@ -40,14 +41,21 @@
         'Successfully uninstalled plugin.' => 'El plugin ha sido desinstalado exitosamente.',
         'The Backup/Restore plugin allows you to create complete backups of the Wolf CMS core database.' => 'El plugin Backup/Restore te permite crear respaldos completos de la base de datos principal de Wolf CMS.',
         'The settings have been saved.' => 'Los ajustes han sido guardados.',
-        'This is an example of the filename that will be used for the generated XML file.' => 'éste es un ejemplo del nombre que se le dará al archivo XML generado.',
+        'This is an example of the filename that will be used for the generated XML file.' => '&eacute;ste es un ejemplo del nombre que se le dar&aacute; al archivo XML generado.',
         'Unable to reconstruct table :tablename.' => 'No se ha podido reconstruir la tabla :tablename.',
         'Unable to remove plugin settings.' => 'No se ha podido eliminar los ajustes del plugin.',
         'Unable to truncate current table :tablename.' => 'No se ha podido truncar la tabla :tablename actual.',
         'Upload file' => 'Subir archivo',
-        'Version' => 'Versión',
-        'Warning!' => '¡Advertencia!',
-        'What style of timestamp should be encorporated into the filename.' => 'El estilo de fecha de registro que será añadido en el nombre del archivo.',
-        'Yes' => 'Sí',
-        'You do not have permission to access the requested page!' => '¡No cuentas con el permiso para acceder a la página que has solicitado!',
+        'Version' => 'Versi&oacute;n',
+        'Warning!' => '&iexcl;Advertencia!',
+        'What style of timestamp should be encorporated into the filename.' => 'El estilo de fecha de registro que ser&aacute; a&ntilde;adido en el nombre del archivo.',
+        'Yes' => 'S&iacute;',
+        'You do not have permission to access the requested page!' => '&iexcl;No cuentas con el permiso para acceder a la p&aacute;gina que has solicitado!',
+        'When restoring a backup, please make sure that the backup file was generated from the same Wolf CMS
+            <em>version</em> as you are restoring it to.' => 'Al restaurar un respaldo, debes asegurarte de que el respaldo fue generado desde una <em>versi&oacute;n</em> de Wolf CMS <strong>igual</strong> a la que deseas restaurar.',
+        'Please be aware that <strong>all</strong> the database tables will be truncated when
+            performing a restore. Truncating a table means that all records in that table are deleted.' => 'Por favor, ten en cuenta que <strong>todas</strong> las tablas de la base de datos ser&aacute;n truncadas. Truncar una tabla significa que todos los registros de dicha tabla se eliminan.',
+        'As such, the contents of your backup file will replace the contents of your Wolf CMS database tables.' => 'Por ende, el contenido de tu archivo de respaldo reemplazar&aacute; el contenido de las tablas de tu base de datos de Wolf CMS.',
+        'Do NOT upload a zip file, only upload a plain text XML file!' => '&iexcl;NO subas un archivo zip, solo puedes subir un archivo de texto XML!',
+        'Upload plain text XML file' => 'Subir archivo de texto XML',
     );
