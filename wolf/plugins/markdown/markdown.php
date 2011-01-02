be18eab73541f3ca2c6a81807cbd6e975661d7d9
Martijn <martijn.niji@gmail.com>
2011-01-02 20:59:02 +0100

    Turning filter system switching into proper JS events

--- wolf/plugins/markdown/markdown.php
+++ wolf/plugins/markdown/markdown.php
@@ -30,6 +30,27 @@ else {
 }
 
 ?>
+
+
+// Wolf CMS Filter Switching system
+$(document).ready(function() {
+    $('.filter-selector').bind('wolfSwitchFilterOut', function(event, filtername, elem) {
+        if (filtername == 'markdown') {
+            elem.markItUpRemove();
+        }
+    });
+    
+    $('.filter-selector').bind('wolfSwitchFilterIn', function(event, filtername, elem) {
+        if (filtername == 'markdown') {
+        alert('test');
+            elem.markItUp(markdownSettings);
+            event.stopPropagation();
+        }
+    });
+});
+
+
+
 // -------------------------------------------------------------------
 // markItUp!
 // -------------------------------------------------------------------
--- wolf/plugins/textile/textile.php
+++ wolf/plugins/textile/textile.php
@@ -31,6 +31,22 @@ else {
 
 ?>
 
+// Wolf CMS Filter Switching system
+$(document).ready(function() {
+    $('.filter-selector').bind('wolfSwitchFilterOut', function(event, filtername, elem) {
+        if (filtername == 'textile') {
+            elem.markItUpRemove();
+        }
+    });
+    
+    $('.filter-selector').bind('wolfSwitchFilterIn', function(event, filtername, elem) {
+        if (filtername == 'textile') {
+        alert('test-textile');
+            elem.markItUp(eval(textileSettings));
+        }
+    });
+});
+
 // -------------------------------------------------------------------
 // markItUp!
 // -------------------------------------------------------------------
