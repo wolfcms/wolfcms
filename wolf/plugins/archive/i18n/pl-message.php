78d25385e352986a0e90c0d038cf9299ce79599e
Martijn <martijn.niji@gmail.com>
2011-01-09 16:01:55 +0100

    Updating polish translation files.

--- wolf/plugins/archive/i18n/pl-message.php
+++ wolf/plugins/archive/i18n/pl-message.php
@@ -3,15 +3,14 @@
     /**
      * Polish file for plugin archive
      *
-     * @package Plugins
+     * @package wolf
      * @subpackage archive
      *
      * @author Piotr Fuz <piotr.fuz@gmail.com> and Pawel Balicki <kilab1@o2.pl>
-     * @version Wolf 0.6.0
+     * @version Wolf 0.7.3
      */
 
     return array(
     	'Archive' => 'Archiwum',
-	'Provides an Archive pagetype behaving similar to a blog or news archive.' => 'Wtyczka dostarcza nowy typ strony - Archiwum. Podobny jest on do bloga, czy archiwum news�w.',
+		'Provides an Archive pagetype behaving similar to a blog or news archive.' => 'Wtyczka dostarcza nowy typ strony - Archiwum. Podobny jest on do bloga, lub archiwum newsów.',
     );
-
