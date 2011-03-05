82d5f97c73680bd331fab868d9fd1555ed53f88e
Martijn <martijn.niji@gmail.com>
2011-03-05 18:31:23 +0100

    Added Node::toSlug() function to replace current wolf.js toSlug() JS function in future releases.

--- wolf/app/models/Node.php
+++ wolf/app/models/Node.php
@@ -25,4 +25,67 @@
  */
 class Node extends Record {
 
+
+    /**
+     * Intended to eventually replace like-wise names JS function from wolf.js
+     * 
+     * Note: this function might undergo a name change in future...
+     *
+     * @param type $string
+     * @return type 
+     */
+    public static function toSlug($string) {
+        $replacements = array();
+        
+        $replacements['áàâą'] = 'a';
+        $replacements['б'] = 'b';
+        $replacements['ćčц'] = 'c';
+        $replacements['дđď'] = 'd';
+        $replacements['éèêëęě'] = 'e';
+        $replacements['ф'] = 'f';
+        $replacements['гѓ'] = 'g';
+        $replacements['íîïи'] = 'i';
+        $replacements['й'] = 'j';
+        $replacements['к'] = 'k';
+        $replacements['łл'] = 'l';
+        $replacements['м'] = 'm';
+        $replacements['ñńň'] = 'n';
+        $replacements['óôó'] = 'o';
+        $replacements['п'] = 'p';
+        $replacements['úùûů'] = 'u';
+        $replacements['ř'] = 'r';
+        $replacements['šś'] = 's';
+        $replacements['ťт'] = 't';
+        $replacements['в'] = 'v';
+        $replacements['ýы'] = 'y';
+        $replacements['žżźз'] = 'z';
+        $replacements['äæ'] = 'ae';
+        $replacements['ч'] = 'ch';
+        $replacements['öø'] = 'oe';
+        $replacements['ü'] = 'ue';
+        $replacements['ш'] = 'sh';
+        $replacements['щ'] = 'shh';
+        $replacements['ß'] = 'ss';
+        $replacements['å'] = 'aa';
+        $replacements['я'] = 'ya';
+        $replacements['ю'] = 'yu';
+        $replacements['ж'] = 'zh';
+        $replacements['[^-a-z0-9~\s\.:;+=_]'] = '';
+        $replacements['[\s\.:;=+]+'] = '-';
+
+        // Test for non western characters
+        // Need to do this in a better way
+        $test = preg_match('([a-z]|[A-Z]|[0-9]|[áàâąбćčцдđďéèêëęěфгѓíîïийкłлмñńňóôóпúùûůřšśťтвýыžżźзäæчöøüшщßåяюж])', $string);
+        if ($test === 0 || false === $test) {
+            return $string;
+        }
+        
+        $string = strtolower(trim($string));
+        
+        foreach ($replacements as $key => $value) {
+            $string = preg_replace('/'.$key.'/', $value, $string);
+        }
+
+        return preg_replace('/[-]+/', '-', $string);
+    }
 }
