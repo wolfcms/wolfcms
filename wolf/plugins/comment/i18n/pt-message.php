8dd27a12230f2a4f4a2ae84608c4fa58fae1f3f8
Martijn <martijn.niji@gmail.com>
2011-04-11 01:58:06 +0200

    Added Portugese language file for comments plugin - thanks aiyumi

---
+++ wolf/plugins/comment/i18n/pt-message.php
@@ -0,0 +1,68 @@
+<?php
+
+    /**
+     * Português-brasileiro language file for plugin comment
+     *
+     * @package Plugins
+     * @subpackage comment
+     *
+     * @author Aiyumi Moriya <aiyumi.br@gmail.com>
+     * @version Wolf 0.7.3
+     */
+
+    return array(
+    	'A comment was added.' => 'Um comentário foi adicionado.',
+	'Approve' => 'Aprovar',
+	'Are you sure you wish to delete it?' => 'Tem certeza de que deseja apagar?',
+	'Auto approve' => 'Aprovação automática',
+	'Body' => 'Corpo',
+	'Cancel' => 'Cancelar',
+	'Choose yes if you want to display the number of to-be-moderated &amp; total number of comment in the tab of the Comment plugin.' => 'Escolha sim se quiser exibir o número de comentários a serem moderados e o total de comentários na aba do plugin.',
+	'Choose yes if you want to use a captcha to protect yourself against spammers.' => 'Escolha sim se quiser usar um captcha para se proteger contra os spammers.',
+	'Choose yes if you want your comments to be auto approved. Otherwise, they will be placed in the moderation queue.' => 'Escolha sim se quiser que os comentários sejam aprovados automaticamente. Caso contrário, eles serão colocados na fila de moderação.',
+	'Closed' => 'Fechado',
+	'Comment has been approved!' => 'O comentário foi aprovado!',
+	'Comment has been deleted!' => 'O comentário foi apagado!',
+	'Comment has been saved!' => 'O comentário foi salvo!',
+	'Comment has been unapproved!' => 'O comentário foi desaprovado!',
+	'Comment has not been deleted!' => 'O comentário não foi apagado!',
+	'Comment has not been saved!' => 'O comentário não foi salvo!',
+	'Comment not found!' => 'Comentário não encontrado!',
+	'Comments' => 'Comentários',
+	'Comments Plugin' => 'Plugin Comentários',
+	'Comments per page' => 'Comentários por página',
+	'Comments settings' => 'Configurações dos comentários',
+	'Delete' => 'Apagar',
+	'Documentation' => 'Documentação',
+	'Edit' => 'Editar',
+	'Edit comment by' => 'Editar comentário por',
+	'Enhance comments tab' => 'Melhorar a aba comentários',
+	'Incorrect result value. Please try again:' => 'Resultado incorreto. Por favor, tente novamente:',
+	'Moderation' => 'Moderação',
+	'Modify' => 'Modificar',
+	'No' => 'Não',
+	'No comments found for moderation.' => 'Nenhum comentário foi encontrado para a moderação.',
+	'No comments found.' => 'Nenhum comentário encontrado.',
+	'Open' => 'Aberto',
+	'Please insert the result of the arithmetical operation from the following image:' => 'Por favor insira o resultado da operação aritmética da imagem a seguir:',
+	'Please insert the result of the arithmetical operation from this image.' => 'Por favor insira o resultado da operação aritmética desta imagem.',
+	'Provides interface to add page comments.' => 'Fornece uma interface para adicionar comentários às páginas.',
+	'Reject' => 'Rejeitar',
+	'Save' => 'Salvar',
+	'Sets the number of comments to be displayed per page in the backend.' => 'Define o número de comentários a serem exibidos por página na área de administração.',
+	'Settings' => 'Configurações',
+	'Successfully uninstalled plugin.' => 'Plugin desinstalado com sucesso.',
+	'Thank you for your comment. It has been added.' => 'Obrigado pelo seu comentário. Ele foi adicionado.',
+	'Thank you for your comment. It is waiting for approval.' => 'Obrigado pelo seu comentário. Ele está aguardando aprovação.',
+	'The settings have been updated.' => 'As configurações foram atualizadas.',
+	'Unable to clean up table alterations.' => 'Não foi possível limpar as alterações da tabela.',
+	'Unable to delete plugin settings.' => 'Não foi possível excluir as configurações do plugin.',
+	'Unable to drop table :tablename' => 'Não foi possível remover a tabela :tablename',
+	'Use captcha' => 'Usar captcha',
+	'Yes' => 'Sim',
+	'You have modified this page.  If you navigate away from this page without first saving your data, the changes will be lost.' => 'Você modificou esta página. Se você sair desta página sem salvar seus dados, as alterações serão perdidas.',
+	'about' => 'sobre',
+	'comment not found!' => 'Comentário não encontrado!',
+	'none' => 'nenhum',
+	'or' => 'ou',
+    );
