5a122167f1c86584ed6b9b18ec23067e50f3e6ec
David <dajare@gmail.com>
2011-05-19 08:00:02 +0100

    Added file mgr pt i18n file: thanks daniel.affonso

---
+++ wolf/plugins/file_manager/i18n/pt-message.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Portuguese (Brazilian) language file for plugin file_manager
+ *
+ * @package Plugins
+ * @subpackage file_manager
+ *
+ * @author Daniel Affonso <daniel.affonso@gmail.com>
+ * @version Wolf 0.7
+ */
+
+return array(
+        'A possible hack attempt was detected.' => 'Poss&iacute;vel tentativa de hack detectada.',
+        'Action' => 'A&ccedil;&atilde;o',
+        'Are you sure you wish to delete?' => 'Confirma a exclus&atilde;o',
+        'Cancel' => 'Cancelar',
+        'Change mode' => 'Mudar permissoes',
+        'Close' => 'Fechar',
+        'Create' => 'Criar',
+        'Create new directory' => 'Criar nova pasta',
+        'Create new file' => 'Criar novo arquivo',
+        'Delete file' => 'Excluir arquivo',
+        'Directory :name has not been created!' => 'Pasta <strong>:name</strong> nao foi criada.',
+        'Directory is not writable! File has not been saved!' => 'Esta Pasta nao tem permissao de escrita. Arquivo nao foi salvo.',
+        'File' => 'Arquivo',
+        'File :name has been created with success!' => 'Arquivo <strong>:name</strong> criado com sucesso.',
+        'File :name has not been created!' => 'Nao foi possivel criar <strong>:name</strong>',
+        'File Manager' => 'Gerenciador de Arquivos',
+        'File has been saved with success!' => 'Arquivo salvo com sucesso',
+        'File has not been uploaded!' => 'O arquivo nao foi enviado',
+        'File is not writable! File has not been saved!' => 'O arquivo nao tem permissao de gravacao. Arquivo nao foi salvo',
+        'File or directory not found!' => 'Arquivo ou Pasta nao encontrados',
+        'Files' => 'Arquivos',
+        'Modify' => 'Modificar',
+        'Modified' => 'Modificado em',
+        'Permission denied!' => 'Permissao Negada',
+        'Permissions' => 'Permissoes',
+        'Provides interface to manage files from the administration.' => 'Interface para gerenciar arquivos',
+        'Rename' => 'Renomear',
+        'Save' => 'Salvar',
+        'Save and Continue Editing' => 'Salvar e continuar editando',
+        'Size' => 'Tamanho',
+        'Upload' => 'Enviar',
+        'Upload file' => 'Enviar arquivo',
+        'You do not have permission to access the requested page!' => 'Voce nao tem permissao para acessar a pagina solicitada',
+        'delete file icon' => 'icone de excluir arquivo',
+        'or' => 'ou',
+        'overwrite it?' => 'salvar sobre arquivo existente?',
+);
