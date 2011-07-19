cd5d2bc55887c5bd3e1c672ff866ed7c25cb235e
Emanuele Alimonda <emanuele@alimonda.com>
2011-07-19 04:11:23 +0200

    Added Italian file for plugin textile

---
+++ wolf/plugins/textile/i18n/it-message.php
@@ -0,0 +1,18 @@
+<?php
+
+    /**
+     * Italian file for plugin textile
+     *
+     * @package Plugins
+     * @subpackage textile
+     *
+     * @author Emanuele Alimonda <emanuele@alimonda.com>
+     * @version Wolf x.y.z
+     */
+
+    return array(
+    	'Allows you to use the Textile text filter.' => 'Consente l\'uso del filtro Textile per il testo.',
+	'Textile' => 'Textile',
+	'Textile filter' => 'Filtro Textile',
+    );
+
