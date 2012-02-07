938ccb13bdb851b4f61c29ad51925629342d2085
Alireza Eliaderani <eliaderani@gmail.com>
2012-02-08 00:55:07 +0330

    removed codeigniter like codes add mimes where it included a non-existing file for it

--- wolf/helpers/Upload.php
+++ wolf/helpers/Upload.php
@@ -54,10 +54,10 @@ class Upload {
     public $image_type     = '';
     public $image_size_str = '';
     public $error_msg      = array();
-    public $mimes          = array();
     public $remove_spaces  = true;
-    public $xss_clean      = false;
-    public $temp_prefix    = "temp_file_";
+    public $xss_clean = false;
+    public $temp_prefix = "temp_file_";
+    public $mimes = array();
 
     /**
      * Constructor
@@ -402,7 +402,7 @@ class Upload {
      * @return  void
      */
     function setImageProperties($path = '') {
-        if ( ! $this->is_image()) {
+        if ( ! $this->is_image) {
             return;
         }
 
@@ -547,7 +547,7 @@ class Upload {
      */
     function validateUploadPath() {
         if ($this->upload_path == '') {
-            $this->setError('upload_no_filepath');
+            $this->setError('upload path is empty');
             return false;
         }
 
@@ -556,7 +556,7 @@ class Upload {
         }
 
         if ( ! @is_dir($this->upload_path)) {
-            $this->setError('upload_no_filepath');
+            $this->setError('upload path is not a directory');
             return false;
         }
 
@@ -656,8 +656,8 @@ class Upload {
 
         $data = fread($fp, filesize($file));
 
-        $CI =& get_instance();
-        $data = $CI->input->xss_clean($data);
+//        $CI =& get_instance();
+//        $data = $CI->input->xss_clean($data);
 
         fwrite($fp, $data);
         flock($fp, LOCK_UN);
@@ -674,16 +674,11 @@ class Upload {
      * @return  void
      */
     function setError($msg) {
-        $CI =& get_instance();
-        $CI->lang->load('upload');
-
         if (is_array($msg)) {
             foreach ($msg as $val) {
-                $msg = ($CI->lang->line($val) == false) ? $val : $CI->lang->line($val);
-                $this->error_msg[] = $msg;
+                $this->error_msg[] = $val;
             }
         } else {
-            $msg = ($CI->lang->line($msg) == false) ? $msg : $CI->lang->line($msg);
             $this->error_msg[] = $msg;
         }
     }
@@ -721,13 +716,103 @@ class Upload {
      */
     function mimesTypes($mime) {
         if (count($this->mimes) == 0) {
-            if (@include(APP_PATH . 'config' . DS . 'mimes.php')) {
-                $this->mimes = $mimes;
-                unset($mimes);
-            }
+            $this->mimes = array('hqx' => 'application/mac-binhex40',
+                'cpt' => 'application/mac-compactpro',
+                'csv' => array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel'),
+                'bin' => 'application/macbinary',
+                'dms' => 'application/octet-stream',
+                'lha' => 'application/octet-stream',
+                'lzh' => 'application/octet-stream',
+                'exe' => array('application/octet-stream', 'application/x-msdownload'),
+                'class' => 'application/octet-stream',
+                'psd' => 'application/x-photoshop',
+                'so' => 'application/octet-stream',
+                'sea' => 'application/octet-stream',
+                'dll' => 'application/octet-stream',
+                'oda' => 'application/oda',
+                'pdf' => array('application/pdf', 'application/x-download'),
+                'ai' => 'application/postscript',
+                'eps' => 'application/postscript',
+                'ps' => 'application/postscript',
+                'smi' => 'application/smil',
+                'smil' => 'application/smil',
+                'mif' => 'application/vnd.mif',
+                'xls' => array('application/excel', 'application/vnd.ms-excel', 'application/msexcel'),
+                'ppt' => array('application/powerpoint', 'application/vnd.ms-powerpoint'),
+                'wbxml' => 'application/wbxml',
+                'wmlc' => 'application/wmlc',
+                'dcr' => 'application/x-director',
+                'dir' => 'application/x-director',
+                'dxr' => 'application/x-director',
+                'dvi' => 'application/x-dvi',
+                'gtar' => 'application/x-gtar',
+                'gz' => 'application/x-gzip',
+                'php' => 'application/x-httpd-php',
+                'php4' => 'application/x-httpd-php',
+                'php3' => 'application/x-httpd-php',
+                'phtml' => 'application/x-httpd-php',
+                'phps' => 'application/x-httpd-php-source',
+                'js' => 'application/x-javascript',
+                'swf' => 'application/x-shockwave-flash',
+                'sit' => 'application/x-stuffit',
+                'tar' => 'application/x-tar',
+                'tgz' => array('application/x-tar', 'application/x-gzip-compressed'),
+                'xhtml' => 'application/xhtml+xml',
+                'xht' => 'application/xhtml+xml',
+                'zip' => array('application/x-zip', 'application/zip', 'application/x-zip-compressed'),
+                'mid' => 'audio/midi',
+                'midi' => 'audio/midi',
+                'mpga' => 'audio/mpeg',
+                'mp2' => 'audio/mpeg',
+                'mp3' => array('audio/mpeg', 'audio/mpg', 'audio/mpeg3', 'audio/mp3'),
+                'aif' => 'audio/x-aiff',
+                'aiff' => 'audio/x-aiff',
+                'aifc' => 'audio/x-aiff',
+                'ram' => 'audio/x-pn-realaudio',
+                'rm' => 'audio/x-pn-realaudio',
+                'rpm' => 'audio/x-pn-realaudio-plugin',
+                'ra' => 'audio/x-realaudio',
+                'rv' => 'video/vnd.rn-realvideo',
+                'wav' => 'audio/x-wav',
+                'bmp' => 'image/bmp',
+                'gif' => 'image/gif',
+                'jpeg' => array('image/jpeg', 'image/pjpeg'),
+                'jpg' => array('image/jpeg', 'image/pjpeg'),
+                'jpe' => array('image/jpeg', 'image/pjpeg'),
+                'png' => array('image/png', 'image/x-png'),
+                'tiff' => 'image/tiff',
+                'tif' => 'image/tiff',
+                'css' => 'text/css',
+                'html' => 'text/html',
+                'htm' => 'text/html',
+                'shtml' => 'text/html',
+                'txt' => 'text/plain',
+                'text' => 'text/plain',
+                'log' => array('text/plain', 'text/x-log'),
+                'rtx' => 'text/richtext',
+                'rtf' => 'text/rtf',
+                'xml' => 'text/xml',
+                'xsl' => 'text/xml',
+                'mpeg' => 'video/mpeg',
+                'mpg' => 'video/mpeg',
+                'mpe' => 'video/mpeg',
+                'qt' => 'video/quicktime',
+                'mov' => 'video/quicktime',
+                'avi' => 'video/x-msvideo',
+                'movie' => 'video/x-sgi-movie',
+                'doc' => 'application/msword',
+                'docx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                'xlsx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                'word' => array('application/msword', 'application/octet-stream'),
+                'xl' => 'application/excel',
+                'eml' => 'message/rfc822',
+                'json' => array('application/json', 'text/json')
+            );
         }
 
-        return ( ! isset($this->mimes[$mime])) ? false : $this->mimes[$mime];
+        return (!isset($this->mimes[$mime])) ? false : $this->mimes[$mime];
     }
 
-} // End Upload Class
+}
+
+// End Upload Class
