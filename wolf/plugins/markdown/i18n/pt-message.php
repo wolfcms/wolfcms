7788d95c7cde21aa4ba88f3cf1194fa38de33b74
Téo Marcus <mac11@Mac11-Teodoro-Sampaio.local>
2012-02-29 20:39:43 -0300

    Portuguese Translation of All Core Plugins Wolf version 0.7.5
    - Comment Plugin: Revised and put special characters in html code.
    - File Manager Plugin: upgraded from 0.7 to 0.7.5 Wolf version.
    - And created translations for the other remaining plugins

---
+++ wolf/plugins/markdown/i18n/pt-message.php
@@ -0,0 +1,18 @@
+<?php
+
+    /**
+     * Português-brasileiro file for plugin markdown
+     *
+     * @package Plugins
+     * @subpackage markdown
+     *
+     * @author Téo Marcus <teo@teomarcus.com>
+     * @link http://www.teomarcus.com
+     * @version Wolf 0.7.5
+     */
+
+    return array(
+    'Allows you to use the Markdown text filter.' => 'Permite a voc&ecirc; usar o filtro de texto Markdown.',
+	'Markdown' => 'Markdown',
+	'Markdown filter' => 'Filtro Markdown',
+    );
