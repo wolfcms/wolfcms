2f6abf9c3d149ce3d19494f49c20e2216ff3ee05
Martijn <martijn.niji@gmail.com>
2012-05-22 17:26:35 +0300

    Making backend display prerequisites errors in plugin listing.

--- wolf/app/views/setting/index.php
+++ wolf/app/views/setting/index.php
@@ -47,9 +47,10 @@
                             $loaded_plugins = Plugin::$plugins;
                             $loaded_filters = Filter::$filters;
                             foreach(Plugin::findAll() as $plugin):
-                                $disabled = (isset($plugin->require_wolf_version) && version_compare($plugin->require_wolf_version, CMS_VERSION, '>'));
+                                $errors = array();
+                                $disabled = !Plugin::hasPrerequisites($plugin, $errors);
                         ?>
-                        <tr<?php if ($disabled) echo ' class="disabled"'; ?>>
+                        <tr<?php if ($disabled === true) echo ' class="disabled"'; ?>>
                             <td class="plugin">
                                 <h4>
                                 <?php
@@ -60,7 +61,7 @@
                                 ?>
                                     <span class="from"><?php if (isset($plugin->author)) echo ' '.__('by').' '.$plugin->author; ?></span>
                                 </h4>
-                                <p><?php echo __($plugin->description); ?> <?php if ($disabled) echo '<span class="notes">'.__('This plugin CANNOT be enabled! It requires Wolf version :v.', array(':v' => $plugin->require_wolf_version)).'</span>'; ?></p>
+                                <p><?php echo __($plugin->description); ?> <?php if ($disabled === true) echo '<span class="notes">'.__('This plugin CANNOT be enabled!<br/>').implode('<br/>', $errors).'</span>'; ?></p>
                             </td>
                             <td class="pluginSettings">
                                 <?php
