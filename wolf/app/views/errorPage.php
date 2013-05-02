eeecf7c4e4b67c89ca2262e719979e30a5e0db99
Martijn <martijn.niji@gmail.com>
2013-05-02 23:58:43 +0200

    Added generic errorPage() function to replace pageNotFound()
    Its error code argument defaults to 404.

--- wolf/app/views/errorPage.php
+++ wolf/app/views/errorPage.php
@@ -1,8 +1,7 @@
 <?php
 /*
  * Wolf CMS - Content Management Simplified. <http://www.wolfcms.org>
- * Copyright (C) 2008-2010 Martijn van der Kleijn <martijn.niji@gmail.com>
- * Copyright (C) 2008 Philippe Archambault <philippe.archambault@gmail.com>
+ * Copyright (C) 2013 Martijn van der Kleijn <martijn.niji@gmail.com>
  *
  * This file is part of Wolf CMS. Wolf CMS is licensed under the GNU GPLv3 license.
  * Please see license.txt for the full license text.
@@ -11,24 +10,26 @@
 /**
  * @package Views
  *
- * @author Philippe Archambault <philippe.archambault@gmail.com>
- * @copyright Philippe Archambault, 2008
+ * @author Martijn van der Kleijn <martijn.niji@gmail.com>
+ * @copyright Martijn van der Kleijn, 2013
  * @license http://www.gnu.org/licenses/gpl.html GPLv3 license
  */
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-  <title>Content Not Found</title>
-  <meta name="description" content="The content you requested was not found." />
-  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-  <meta name="generator" content="wolf-cms" />
-</head>
-<body>
-  <h1>Content Not Found (404)</h1>
-  <p>The content you requested was not found. It may have been deleted or you may have entered an incorrect address.</p>
-  <p>Please return to the <a href="<?php echo URL_PUBLIC; ?>">home page</a> to view the main navigation.</p>
-</body>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title><?php echo $message; ?></title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="generator" content="Wolf CMS" />
+    </head>
+    <body>
+        <h1><?php echo $message . ' (' . $code . ')'; ?></h1>
+        <?php
+        if (isset($longMessage)) {
+            echo $longMessage;
+        }
+        ?>
+    </body>
 </html>
 
 <!--
@@ -36,4 +37,6 @@
    - If the text of an error's message is 'too small', specifically less than 512 bytes, 
    - Internet Explorer returns its own error message. You can turn that off, but it's 
    - tricky to find. This comment serves as padding to prevent that behaviour in IE.
+   -
+   - I really don't like Microsoft sometimes...
 -->
