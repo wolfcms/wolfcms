f835fb028acded05e7aeba26200f060a629f9c42
Nic Wortel <nic-nld@live.nl>
2014-01-16 00:02:38 +0100

    Updated Page edit screen to use the new updater() method

--- wolf/app/views/page/edit.php
+++ wolf/app/views/page/edit.php
@@ -200,7 +200,7 @@ if ($action == 'edit') { ?>
     
     <p><small>
 <?php if (isset($page->updated_on)): ?>
-    <?php echo __('Last updated by :username on :date', array( ':username' => $page->updated_by_name, ':date' => date('D, j M Y', strtotime($page->updated_on)) )); ?>
+    <?php echo __('Last updated by :username on :date', array( ':username' => $page->updater()->name, ':date' => date('D, j M Y', strtotime($page->updated_on)) )); ?>
 <?php endif; ?>
     &nbsp;
     </small></p>
