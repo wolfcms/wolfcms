131758b7ac687a1e770ce25a445ddc2a9d23a360
andrés <andrewmman@gmail.com>
2014-04-10 14:01:05 +0200

    remove legacy functions (Prototype)

--- wolf/admin/javascripts/wolf.js
+++ wolf/admin/javascripts/wolf.js
@@ -68,32 +68,6 @@ function page_id() {
     }
 }
 
-// When object is available, do function fn.
-function when(obj, fn) {
-  if (Object.isString(obj)) obj = /^[\w-]+$/.test(obj) ? $(obj) : $(document.body).down(obj);
-  if (Object.isArray(obj) && !obj.length) return;
-  if (obj) fn(obj);
-}
-
-function part_added() {
-  var partNameField = $('part-name-field');
-  var partIndexField = $('part-index-field');
-  var index = parseInt(partIndexField.value);
-  tabControl.addTab('tab-' + index,  partNameField.value, 'page-' + index);
-  Element.toggle('busy');
-  Element.hide('add-part-popup');
-  partNameField.value = '';
-  partIndexField.value = (index + 1).toString();
-  $('add-part-button').disabled = false;
-  Field.focus(partNameField);
-  tabControl.select(tab);
-}
-
-function part_loading() {
-  $('add-part-button').disabled = true;
-  Element.toggle('busy');
-}
-
 // Updated valid_part_name function for JQuery
 function valid_part_name(name) {
   name = name.toLowerCase();
@@ -114,16 +88,6 @@ function valid_part_name(name) {
   })
 
   return result;
-}
-
-function center(element) {
-  var header = $('header')
-  element = $(element);
-  element.style.position = 'absolute';
-  var dim = Element.getDimensions(element);
-  var top = document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop;
-  element.style.top = (top + 200) + 'px';
-  element.style.left = ((header.offsetWidth - dim.width) / 2) + 'px';
 }
 
 var toggle_reorder = false;
