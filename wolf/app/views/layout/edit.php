57b78ae4a6d550b8662a0ba09020d48704d261f6
Dejan Andjelkovic <dejan79@gmail.com>
2014-10-22 15:04:41 +0200

    Layouts done (check javascript)

--- wolf/app/views/layout/edit.php
+++ wolf/app/views/layout/edit.php
@@ -17,37 +17,39 @@
  * @license http://www.gnu.org/licenses/gpl.html GPLv3 License
  */
 ?>
-<h1><?php echo __(ucfirst($action).' layout'); ?></h1>
+<h2><?php echo __(ucfirst($action).' layout'); ?></h2>
 
-<form action="<?php echo $action=='edit' ? get_url('layout/edit/'. $layout->id): get_url('layout/add'); ; ?>" method="post">
+<div class="panel panel-default">
+  
+<form role="form" action="<?php echo $action=='edit' ? get_url('layout/edit/'. $layout->id): get_url('layout/add'); ; ?>" method="post">
     <input id="csrf_token" name="csrf_token" type="hidden" value="<?php echo $csrf_token; ?>" />
-  <div class="form-area">
-    <p class="title">
-      <label for="layout_name"><?php echo __('Name'); ?></label>
-      <input class="textbox" id="layout_name" maxlength="100" name="layout[name]" size="100" type="text" value="<?php echo $layout->name; ?>" />
-    </p>
-    
-    <table class="fieldset" cellpadding="0" cellspacing="0" border="0">
-      <tr>
-        <td><label for="layout_content_type"><?php echo __('Content-Type'); ?></label></td>
-        <td class="field"><input class="textbox" id="layout_content_type" maxlength="40" name="layout[content_type]" size="40" type="text" value="<?php echo $layout->content_type; ?>" /></td>
-      </tr>
-    </table>
-    
-    <p class="content">
-      <label for="layout_content"><?php echo __('Body'); ?></label>
-      <textarea class="textarea" cols="40" id="layout_content" name="layout[content]" rows="20" style="width: 100%"><?php echo htmlentities($layout->content, ENT_COMPAT, 'UTF-8'); ?></textarea>
-    </p>
-<?php if (isset($layout->updated_on)) { ?>
-    <p style="clear: left"><small><?php echo __('Last updated by'); ?> <?php echo $layout->updated_by_name; ?> <?php echo __('on'); ?> <?php echo date('D, j M Y', strtotime($layout->updated_on)); ?></small></p>
-<?php } ?>
-  </div>
-  <p class="buttons">
-    <input class="button" name="commit" type="submit" accesskey="s" value="<?php echo __('Save'); ?>" />
-    <input class="button" name="continue" type="submit" accesskey="e" value="<?php echo __('Save and Continue Editing'); ?>" />
-    <?php echo __('or'); ?> <a href="<?php echo get_url('layout'); ?>"><?php echo __('Cancel'); ?></a>
-  </p>
-</form>
+
+    <div class="panel-body">
+        <div class="form-group">
+            <label for="name"><?php echo __('Name'); ?></label>
+            <input class="form-control" id="layout_name" maxlength="100" name="layout[name]" size="100" type="text" value="<?php echo $layout->name; ?>" />      
+        </div>
+        <div class="form-group form-inline">
+            <label for="layout_content_type"><?php echo __('Content-Type'); ?></label>
+            <input class="form-control" id="layout_content_type" maxlength="40" name="layout[content_type]" size="40" type="text" value="<?php echo $layout->content_type; ?>" />
+        </div>
+        <div class="form-group">
+            <label for="layout_content"><?php echo __('Body'); ?></label>
+            <textarea class="form-control" cols="40" id="layout_content" name="layout[content]" rows="20" style="width: 100%"><?php echo htmlentities($layout->content, ENT_COMPAT, 'UTF-8'); ?></textarea>
+        </div>
+
+        <?php if (isset($layout->updated_on)) { ?>
+            <p><small><?php echo __('Last updated by'); ?> <?php echo $layout->updated_by_name; ?> <?php echo __('on'); ?> <?php echo date('D, j M Y', strtotime($layout->updated_on)); ?></small></p>
+        <?php } ?>
+        <p>
+            <button class="btn btn-primary" name="commit" type="submit" accesskey="s" value="<?php echo __('Save'); ?>"><?php echo __('Save'); ?></button>
+            <button class="btn btn-primary" name="continue" type="submit" accesskey="e" value="<?php echo __('Save and Continue Editing'); ?>"><?php echo __('Save and Continue Editing'); ?></button>
+            <?php echo __('or'); ?> <a href="<?php echo get_url('layout'); ?>"><?php echo __('Cancel'); ?></a>
+        </p>
+
+    </div>
+
+</div>
 
 <script type="text/javascript">
 // <![CDATA[
