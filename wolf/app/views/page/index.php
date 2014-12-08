5f89fbfad6156f66c0b4c13ebc9c0f48552cd747
Dejan Andjelkovic <dejan79@gmail.com>
2014-12-08 15:17:07 +0100

    Playing with FontAwesome icons on Page list

--- wolf/app/views/page/index.php
+++ wolf/app/views/page/index.php
@@ -31,9 +31,9 @@
       <div class="page" style="padding-left: 4px">
         <span class="w1">
 <?php if (!AuthUser::hasPermission('page_edit') || (!AuthUser::hasPermission('admin_edit') && $root->is_protected)): ?>
-          <img align="middle" class="icon" src="<?php echo PATH_PUBLIC;?>wolf/admin/images/page.png" alt="page icon" /> <span class="title"><?php echo $root->title; ?></span>
+          <i class="fa fa-file-o"></i> <span class="title"><?php echo $root->title; ?></span>
 <?php else: ?>
-          <a href="<?php echo get_url('page/edit/1'); ?>" title="/"><img align="middle" class="icon" src="<?php echo PATH_PUBLIC;?>wolf/admin/images/page.png" alt="page icon" /> <span class="title"><?php echo $root->title; ?></span></a>
+          <a href="<?php echo get_url('page/edit/1'); ?>" title="/"><i class="fa fa-file-o"></i> <span class="title"><?php echo $root->title; ?></span></a>
 <?php endif; ?>
         </span>
       </div>
@@ -41,9 +41,9 @@
       <div class="status published-status"><?php echo __('Published'); ?></div>
       <div class="view-page"><a href="<?php echo URL_PUBLIC; ?>" target="_blank"><img src="<?php echo PATH_PUBLIC;?>wolf/admin/images/magnify.png" align="middle" alt="<?php echo __('View Page'); ?>" title="<?php echo __('View Page'); ?>" /></a></div>
       <div class="modify">
-          <a href="<?php echo get_url('page/add/1'); ?>"><img src="<?php echo PATH_PUBLIC;?>wolf/admin/images/plus.png" align="middle" title="<?php echo __('Add child'); ?>" alt="<?php echo __('Add child'); ?>" /></a>&nbsp;
-          <img class="remove" src="<?php echo PATH_PUBLIC;?>wolf/admin/images/icon-remove-disabled.gif" align="middle" alt="<?php echo __('remove icon disabled'); ?>" title="<?php echo __('Remove unavailable'); ?>"/>&nbsp;
-      	  <img src="<?php echo PATH_PUBLIC;?>wolf/admin/images/copy-disabled.png" align="middle" title="<?php echo __('Copy Page Disabled'); ?>" alt="<?php echo __('Copy Page Disabled'); ?>" />
+          <a href="<?php echo get_url('page/add/1'); ?>" title="<?php echo __('Add child'); ?>"><i class="fa fa-plus-square"></i></a>&nbsp;
+          <i class="fa fa-minus-square remove"></i>&nbsp;
+      	  <i class="fa fa-copy"></i>
       </div>
 
 <?php echo $content_children; ?>
