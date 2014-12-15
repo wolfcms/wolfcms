e775764c7ce4c7755f1e0dc4499180eb141e9783
Dejan Andjelkovic <dejan79@gmail.com>
2014-12-15 14:49:45 +0100

    Changed button

--- wolf/app/views/layout/sidebar.php
+++ wolf/app/views/layout/sidebar.php
@@ -18,11 +18,11 @@
 ?>
 <?php if (Dispatcher::getAction() == 'index'): ?>
 
-<p class="button"><a href="<?php echo get_url('layout/add'); ?>"><img src="<?php echo PATH_PUBLIC;?>wolf/admin/images/layout.png" align="middle" alt="layout icon" /> <?php echo __('New Layout'); ?></a></p>
+<a class="button large wide" href="<?php echo get_url('layout/add'); ?>"><i class="fa fa-plus"></i> <?php echo __('New Layout'); ?></a>
 
 <div class="box">
-<h2><?php echo __('What is a Layout?'); ?></h2>
-<p><?php echo __('Use layouts to apply a visual look to a Web page. Layouts can contain special tags to include page content and other elements such as the header or footer. Click on a layout name below to edit it or click <strong>Remove</strong> to delete it.'); ?></p>
+	<h2><?php echo __('What is a Layout?'); ?></h2>
+	<p><?php echo __('Use layouts to apply a visual look to a Web page. Layouts can contain special tags to include page content and other elements such as the header or footer. Click on a layout name below to edit it or click <strong>Remove</strong> to delete it.'); ?></p>
 </div>
 
 <?php endif; ?>
