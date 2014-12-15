fcccb54cf84b5bf2a58b74e28bfae654e6eae908
Dejan Andjelkovic <dejan79@gmail.com>
2014-12-15 14:49:11 +0100

    Changed buttons to new ones, added correct label for  content-type input

--- wolf/app/views/layout/edit.php
+++ wolf/app/views/layout/edit.php
@@ -21,30 +21,26 @@
 
 <form action="<?php echo $action=='edit' ? get_url('layout/edit/'. $layout->id): get_url('layout/add'); ; ?>" method="post">
     <input id="csrf_token" name="csrf_token" type="hidden" value="<?php echo $csrf_token; ?>" />
-  <div class="form-area">
-    <p class="title">
-      <label for="layout_name"><?php echo __('Name'); ?></label>
-      <input class="textbox" id="layout_name" maxlength="100" name="layout[name]" size="100" type="text" value="<?php echo $layout->name; ?>" />
-    </p>
-    
-    <table class="fieldset" cellpadding="0" cellspacing="0" border="0">
-      <tr>
-        <td><label for="layout_content_type"><?php echo __('Content-Type'); ?></label></td>
-        <td class="field"><input class="textbox" id="layout_content_type" maxlength="40" name="layout[content_type]" size="40" type="text" value="<?php echo $layout->content_type; ?>" /></td>
-      </tr>
-    </table>
-    
-    <p class="content">
-      <label for="layout_content"><?php echo __('Body'); ?></label>
-      <textarea class="textarea" cols="40" id="layout_content" name="layout[content]" rows="20" style="width: 100%"><?php echo htmlentities($layout->content, ENT_COMPAT, 'UTF-8'); ?></textarea>
-    </p>
-<?php if (isset($layout->updated_on)) { ?>
-    <p style="clear: left"><small><?php echo __('Last updated by'); ?> <?php echo $layout->updated_by_name; ?> <?php echo __('on'); ?> <?php echo date('D, j M Y', strtotime($layout->updated_on)); ?></small></p>
-<?php } ?>
-  </div>
+      <div class="form-area">
+        <p class="title">
+            <label for="layout_name"><?php echo __('Name'); ?></label>
+            <input class="textbox" id="layout_name" maxlength="100" name="layout[name]" size="100" type="text" value="<?php echo $layout->name; ?>" />
+        </p>
+        <p class="content-type">
+            <label for="layout_content_type"><?php echo __('Content-Type'); ?></label>
+            <input class="textbox" id="layout_content_type" maxlength="40" name="layout[content_type]" size="40" type="text" value="<?php echo $layout->content_type; ?>" />
+        </p>
+        <p class="content">
+            <label for="layout_content"><?php echo __('Body'); ?></label>
+            <textarea class="textarea" id="layout_content" name="layout[content]"><?php echo htmlentities($layout->content, ENT_COMPAT, 'UTF-8'); ?></textarea>
+        </p>
+    <?php if (isset($layout->updated_on)) { ?>
+        <p><small><?php echo __('Last updated by'); ?> <?php echo $layout->updated_by_name; ?> <?php echo __('on'); ?> <?php echo date('D, j M Y', strtotime($layout->updated_on)); ?></small></p>
+    <?php } ?>
+      </div>
   <p class="buttons">
-    <input class="button" name="commit" type="submit" accesskey="s" value="<?php echo __('Save'); ?>" />
-    <input class="button" name="continue" type="submit" accesskey="e" value="<?php echo __('Save and Continue Editing'); ?>" />
+    <button name="commit" type="submit" accesskey="s"><?php echo __('Save'); ?></button>
+    <button name="continue" type="submit" accesskey="e"><?php echo __('Save and Continue Editing'); ?></button>
     <?php echo __('or'); ?> <a href="<?php echo get_url('layout'); ?>"><?php echo __('Cancel'); ?></a>
   </p>
 </form>
