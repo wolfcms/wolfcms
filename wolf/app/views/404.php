9c9249f8da4d5a1617790d6ad45edaf3f096be46
Dejan Andjelkovic <dejan79@gmail.com>
2015-02-04 13:38:45 +0100

    Updated to html5

--- wolf/app/views/404.php
+++ wolf/app/views/404.php
@@ -16,13 +16,13 @@
  * @license http://www.gnu.org/licenses/gpl.html GPLv3 license
  */
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!doctype html>
+<html lang="en">
 <head>
   <title>Content Not Found</title>
-  <meta name="description" content="The content you requested was not found." />
-  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-  <meta name="generator" content="wolf-cms" />
+  <meta name="description" content="The content you requested was not found.">
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <meta name="generator" content="wolf-cms">
 </head>
 <body>
   <h1>Content Not Found (404)</h1>
