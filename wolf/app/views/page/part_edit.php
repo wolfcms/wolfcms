3f5f7f8b58b9b6263487fd69a93975f12ed49c19
Dejan Andjelkovic <dejan79@gmail.com>
2015-02-11 15:01:35 +0100

    Filter name and dropdown surrounded with <label>

--- wolf/app/views/page/part_edit.php
+++ wolf/app/views/page/part_edit.php
@@ -24,13 +24,14 @@
 	    <input id="part_<?php echo ($index-1); ?>_id" name="part[<?php echo ($index-1); ?>][id]" type="hidden" value="<?php echo $page_part->id; ?>" />
 	    <?php endif; ?>
 	    <p>
-	      <label for="part_<?php echo ($index-1); ?>_filter_id"><?php echo __('Filter'); ?></label>
+	      <label for="part_<?php echo ($index-1); ?>_filter_id"><?php echo __('Filter'); ?>
 	      <select id="part_<?php echo ($index-1); ?>_filter_id" class="filter-selector" name="part[<?php echo ($index-1); ?>][filter_id]">
 	        <option value=""<?php if ($page_part->filter_id == '') echo ' selected="selected"'; ?>>&#8212; <?php echo __('none'); ?> &#8212;</option>
 	<?php foreach (Filter::findAll() as $filter): ?> 
 	        <option value="<?php echo $filter; ?>"<?php if ($page_part->filter_id == $filter) echo ' selected="selected"'; ?>><?php echo Inflector::humanize($filter); ?></option>
 	<?php endforeach; ?> 
 	      </select>
+          </label>
 	    </p>
 	    <div>
 	    	<textarea class="textarea markitup<?php if($page_part->filter_id != "") { echo ' '.$page_part->filter_id; } ?>" id="part_<?php echo ($index-1); ?>_content" name="part[<?php echo ($index-1); ?>][content]" style="width: 100%" rows="20" cols="40"><?php echo htmlentities($page_part->content, ENT_COMPAT, 'UTF-8'); ?></textarea>
